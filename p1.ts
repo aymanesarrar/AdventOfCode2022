e57f5192ae6046a0bf85f450e2b92ac9c95dd82d
aymane <aymensarrar1@gmail.com>
2022-12-01 12:28:48 +0100

    first puzzle

---
+++ p1.ts
@@ -0,0 +1,13 @@
+import fs from "fs/promises";
+const processData = async () => {
+    const data = await fs.readFile("input.txt", {encoding: "utf-8"});
+   return Buffer.from(data);
+}
+const sum = (arr: string[]) => {
+    return arr.reduce((prev, curr) => Number(prev) + Number(curr), 0)
+}
+(async () => {
+    const data = (await processData()).toString("utf-8").split(/\n\s*\n/);
+   const sums = data.map(element => sum(element.split("\n")))
+   console.log(Math.max(...sums))
+})()
